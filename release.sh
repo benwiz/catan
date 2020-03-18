6ad2e97dbd6d8e1475f02fe0440887aa68bc67c3
Ben Wiz <benwiz@Bens-MacBook-Air.local>
2020-03-17 23:55:33 -0500

    exclamation

--- release.sh
+++ release.sh
@@ -2,6 +2,8 @@
 
 yarn release
 filename=$(jq '.[0]["output-name"]' docs/js/compiled/manifest.json)
+filename="${filename%\"}" # strip first quote
+filename="${filename#\"}" # strip last quote
 echo "new filename is: $filename"
-sed -i .bak "s/main\..*\.js/main\.$filename\.js/" docs/index.html
+sed -i .bak "s/main\..*\.js/$filename/" docs/index.html
 cat docs/index.html
